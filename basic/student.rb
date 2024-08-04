547e91cfc7b7508f56b1b79a9664e97c9fab8e61
apiwathantrakool <nekky.apiwat@gmail.com>
2024-08-04 14:54:39 +0700

    basic: class

---
+++ basic/student.rb
@@ -0,0 +1,30 @@
+class Student
+    attr_accessor :first_name, :last_name, :email, :username, :password
+    attr_reader :student_id
+
+    def initialize(firstname, lastname, username, email, password)
+      random_id = random_number = rand(1000..10000)
+      @first_name = firstname
+      @last_name = lastname
+      @username = username
+      @email = email
+      @password = password
+      @student_id = random_id
+    end
+   
+    def to_s
+      "ID: #{@student_id}, First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                    email address: #{@email}"
+    end
+   
+  end
+   
+  mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
+                        "password1")
+  john = Student.new("John", "Doe", "john1", "john1@example.com",
+                        "password2")
+  puts mashrur
+  puts john
+  mashrur.last_name = john.last_name
+  puts "Mashrur is altered"
+  puts mashrur
