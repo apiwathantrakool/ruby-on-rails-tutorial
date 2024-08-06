9fab303d6c464e4d9a72bf7d4c40681eb2fdbaae
apiwathantrakool <nekky.apiwat@gmail.com>
2024-08-06 18:22:30 +0700

    basic: adding main

--- basic/student.rb
+++ basic/student.rb
@@ -21,14 +21,3 @@ class Student
     end
    
   end
-
-     
-mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
-  "password1")
-john = Student.new("John", "Doe", "john1", "john1@example.com",
-  "password2")
-
-hashed_password = mashrur.create_hash_digest(mashrur.password)
-
-puts hashed_password
-puts mashrur
