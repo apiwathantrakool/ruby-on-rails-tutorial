f3184d7b31d29f5678e64b2c767c52dc9043811a
apiwathantrakool <nekky.apiwat@gmail.com>
2024-09-09 18:43:11 +0700

    remove duplicated code

--- myapp/app/controllers/articles_controller.rb
+++ myapp/app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
-    @article = Article.find(params[:id])
+    # Get from before_action => set_article
   end
 
   # GET /articles/new
